3193f9ada14968ca518a20ac5afa67ec9c6f4c8b
Felipe Aguirre Martinez <felipeam86@gmail.com>
2018-02-25 18:27:01 +0100

    Test that neccessary directories are created

--- tests/test_ImageDownloader.py
+++ tests/test_ImageDownloader.py
@@ -1,5 +1,8 @@
 # -*- coding: utf-8 -*-
 
+from pathlib import Path
+from tempfile import TemporaryDirectory
+
 import pytest
 
 from imagedownloader.downloader import ImageDownloader
@@ -42,3 +45,41 @@ def test_thumbs_size_init():
     with_thumbs_size_list = ImageDownloader(thumbs_size=[50, 100], thumbs=True)
     assert with_thumbs_size_list.thumbs_size == {'50': (50, 50), '100': (100, 100)}, \
         "When thumbs_size is a list, it should be converted to a dictionary"
+
+
+def test_make_dirs_no_thumbs():
+
+    store_path = TemporaryDirectory()
+    store_path.cleanup()
+    assert not Path(store_path.name).exists()
+
+    _ = ImageDownloader(store_path=store_path.name)
+    assert Path(store_path.name).exists(),\
+        "store_path should have been created"
+
+    store_path.cleanup()
+
+
+def test_make_dirs_with_thumbs():
+
+    store_path = TemporaryDirectory()
+    store_path.cleanup()
+    assert not Path(store_path.name).exists()
+
+    _ = ImageDownloader(
+        store_path=store_path.name,
+        thumbs=True,
+        thumbs_size=[10, 20]
+    )
+    assert Path(store_path.name).exists(),\
+        "{store_path} should have been created"
+
+    assert Path(store_path.name, 'thumbs', '10').exists(),\
+        "{store_path}/thumbs/10 should have been created"
+
+    assert Path(store_path.name, 'thumbs', '20').exists(),\
+        "{store_path}/thumbs/10 should have been created"
+
+    store_path.cleanup()
+
+
