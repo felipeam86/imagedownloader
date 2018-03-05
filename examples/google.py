9695e4910b0e27709157497e5281eb25d04d322f
Felipe Aguirre Martinez <felipeam86@gmail.com>
2018-03-06 00:25:36 +0100

    Control how many images are tried to download

--- examples/google.py
+++ examples/google.py
@@ -14,6 +14,7 @@ IMAGE_STORE = Path(__file__).parent / 'images'
 
 CHROME_DRIVER = Path(__file__).parent / 'chromedriver'
 CHROME_DRIVER_DOWNLOAD_PAGE = "https://sites.google.com/a/chromium.org/chromedriver/downloads"
+MAX_RETRIES = 3
 
 if not CHROME_DRIVER.exists():
     raise FileNotFoundError(f"'chromedriver' executable not found. "
@@ -35,10 +36,11 @@ def get_driver(headless=True):
     return driver
 
 
-def scroll_down(driver):
-    smr = driver.find_element_by_id("smb")
-    if smr.is_displayed():
-        smr.click()
+def scroll_down(driver, click_more_results=False):
+    if click_more_results:
+        smr = driver.find_element_by_id("smb")
+        if smr.is_displayed():
+            smr.click()
     else:
         driver.execute_script("window.scrollTo(0, document.body.scrollHeight);")
 
@@ -62,14 +64,34 @@ def main(args):
     elem.send_keys(args.query)
     elem.send_keys(Keys.RETURN)
 
+    urls = get_urls(driver.page_source)
+    previous_n = new_n = len(urls)
+
     # TODO This is not working very well yet... Find a way to know when page finished loading
     # Se this: http://selenium-python.readthedocs.io/waits.html#implicit-waits
     print("Scrolling down five times")
-    for i in range(5):
-        scroll_down(driver)
-        sleep(2)
-
-    urls = get_urls(driver.page_source)
+    enough = False
+    current_retries = 0
+    n_scrolls = 0
+    # Scroll down until there are enough images or unsuccessful retries exceeded maximum retries
+    while (new_n < args.n_images) and (current_retries < MAX_RETRIES):
+        scroll_down(
+            driver,
+            click_more_results=(new_n == previous_n) and (current_retries != 0)
+        )
+        n_scrolls += 1
+        print(f"Scrolled {n_scrolls} times already")
+        current_retries += 1
+        # Do incremental waits until more images appear
+        for i in range(4):
+            sleep(0.5 * i + 1)
+            urls = get_urls(driver.page_source)
+            new_n = len(urls)
+            if new_n > previous_n:
+                current_retries = 0
+                print(f"{new_n} images so far")
+                break
+        previous_n = new_n
 
     print(f"{len(urls)} images found.")
     print(f"Downloading to {IMAGE_STORE}")
@@ -94,6 +116,9 @@ if __name__ == '__main__':
 
     parser.add_argument('query', type=str,
                         help="Query string to be executed on google images")
+
+    parser.add_argument('-n', '--n_images', type=int, default=100,
+                        help="Number of expected images to download")
 
     parser.add_argument('-f', '--force', action='store_true',
                         help="Force the download even if the files already exists")
