6dca3bcefa34f9432c55512abb0cb9eb481171bc
Felipe Aguirre Martinez <felipeam86@gmail.com>
2023-04-29 19:04:31 -0500

    Skip some tests in github actions to avoid google client connection

--- tests/test_storage_backend.py
+++ tests/test_storage_backend.py
@@ -1,3 +1,4 @@
+import os
 from unittest.mock import patch
 
 import pytest
@@ -5,12 +6,18 @@ import pytest
 from imgdl.storage.backend import GoogleStorage, LocalStorage, resolve_storage_backend
 from imgdl.storage.gcloud import Bucket, Client
 
+IN_GITHUB_ACTIONS = os.getenv("GITHUB_ACTIONS") == "true"
+
 
 class TestResoveBackend:
     def test_local_storage(self, tmp_path):
         storage = resolve_storage_backend(tmp_path)
         assert isinstance(storage, LocalStorage)
 
+    @pytest.mark.skipif(
+        IN_GITHUB_ACTIONS,
+        reason="Google cloud storage Client will not connect in github actions",
+    )
     def test_google_storage_with_bucket_only(self, tmp_path):
         with patch("google.cloud.storage.Bucket.exists", spec=Bucket):
             with patch("imgdl.storage.gcloud.Client", spec=Client):
@@ -19,6 +26,10 @@ class TestResoveBackend:
                 assert storage.bucket_name == "non_existing_bucket_12345"
                 assert storage.bucket_path == ""
 
+    @pytest.mark.skipif(
+        IN_GITHUB_ACTIONS,
+        reason="Google cloud storage Client will not connect in github actions",
+    )
     def test_google_storagewith_bucket_and_path(self, tmp_path):
         with patch("google.cloud.storage.Bucket.exists", spec=Bucket):
             with patch("google.cloud.storage.Client", spec=Client):
